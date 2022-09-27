7e3bdb68cca8033d4f02913cd3a6dd77c4d3f00d
Ivan <ivanbrazas@gmail.com>
2022-09-27 20:20:17 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env(
+        "DATABASE_HOST",
+        "dpg-ccpljv6n6mpg6kdi4s00-a.oregon-postgres.render.com"
+      ),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "guitarla_zozd"),
+      user: env("DATABASE_USERNAME", "root"),
+      password: env("DATABASE_PASSWORD", "sR6SBPYEOtvRzAulDjcxmgA2AalaZVIN"),
+      ssl: env.bool("DATABASE_SSL", true),
+    },
+  },
+});
